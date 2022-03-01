f7f85892161840d719ebe786925f36496ad5d240
FedorViest <fedo.viest@gmail.com>
2022-03-01 20:38:30 +0100

    Try-catch block on KeyboardInterrupt so no errors are shown on program stopping

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ import sipproxy
 # Library from: https://github.com/tirfil/PySipFullProxy/blob/master/sipfullproxy.py
 
 if __name__ == "__main__":
-    HOST, PORT = '192.168.0.115', 6000
+    HOST, PORT = '192.168.50.132', 6000
     sipproxy.recordroute = "Record-Route: <sip:%s:%d;lr>" % (HOST, PORT)
     sipproxy.topvia = "Via: SIP/2.0/UDP %s:%d" % (HOST, PORT)
     server = socketserver.UDPServer((HOST, PORT), sipproxy.UDPHandler)
-    server.serve_forever()
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt as e:
+        print("Server stopping")
